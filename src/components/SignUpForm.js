dd34a986d6447cc8cc86c08be61120d5965b7b14
cappscr <capps.christopher@gmail.com>
2024-03-23 20:16:07 -0400

    Initial Sign Up Form with email and password fields

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,16 +1,46 @@
 import Form from 'react-bootstrap/Form';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 export const SignUpForm = () => {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formGroupEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formGroupPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-    </Form>
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validationSchema={Yup.object({
+        email: Yup.string().email('Invalid email address').required('Required'),
+        password: Yup.string().required('Required'),
+      })}
+    >
+      {({ handleSubmit, handleChange, handleBlur, values, touched, errors }) => (
+        <Form onSubmit={handleSubmit}>
+          <Form.Label htmlFor="email">Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            isValid={touched.email && !errors.email}
+            isInvalid={touched.email && !!errors.email}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.email}
+          </Form.Control.Feedback>
+          <Form.Label htmlFor="password">Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            isValid={touched.password && !errors.password}
+            isInvalid={touched.password && !!errors.password}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.password}
+          </Form.Control.Feedback>
+        </Form>
+      )}
+    </Formik>
   );
 }
