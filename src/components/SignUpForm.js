2145e3355c9883b50d2134fd4e34fdf2b7494ef9
cappscr <capps.christopher@gmail.com>
2024-03-24 06:03:19 -0400

    Username field

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -8,11 +8,27 @@ export const SignUpForm = () => {
       initialValues={{ email: '', password: '' }}
       validationSchema={Yup.object({
         email: Yup.string().email('Invalid email address').required('Required'),
+        firstName: Yup.string().required(),
+        lastName: Yup.string().required(),
         password: Yup.string().min(8, 'Password must be at least 8 characters').matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^$*.[\]{}()?\-"!@#%&/\\,><':;|_~`+=]).{8,}$/, 'Password must contain at least 1 number, 1 uppercase letter, 1 lowercase letter, and 1 special character').required('Required'),
+        username: Yup.string().required(),
       })}
     >
       {({ handleSubmit, handleChange, handleBlur, values, touched, errors }) => (
         <Form onSubmit={handleSubmit}>
+          <Form.Label htmlFor="email">Username</Form.Label>
+          <Form.Control
+            type="text"
+            name="username"
+            value={values.username}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            isValid={touched.username && !errors.username}
+            isInvalid={touched.username && !!errors.username}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.username}
+          </Form.Control.Feedback>
           <Form.Label htmlFor="email">Email Address</Form.Label>
           <Form.Control
             type="email"
