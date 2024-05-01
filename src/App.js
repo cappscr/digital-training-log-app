bb673591eb45cb3656fef35c4a031a7b34284e4a
cappscr <capps.christopher@gmail.com>
2024-05-01 12:02:39 -0400

    Added firebase sdk to React App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import { Routes } from './Routes';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyDFREYIbs_4MbQ0evopMmEeyNRzPHFEnGM",
+    authDomain: "digital-training-log-app.firebaseapp.com",
+    projectId: "digital-training-log-app",
+    storageBucket: "digital-training-log-app.appspot.com",
+    messagingSenderId: "249849079109",
+    appId: "1:249849079109:web:972f47b64653ac40f80a2c",
+  };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  
   return (
     <div className="App">
       <Routes />
