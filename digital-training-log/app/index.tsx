b8991a98416faaeebbacb4e80784a31755562889
cappscr <capps.christopher@gmail.com>
2024-09-28 06:06:21 -0400

    DTL-25 Added box component

--- digital-training-log/app/index.tsx
+++ digital-training-log/app/index.tsx
@@ -1,15 +1,13 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { Text } from "react-native";
+import { Box } from "../components/ui/box";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <Box
+      className="flex-1"
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    </Box>
   );
 }
