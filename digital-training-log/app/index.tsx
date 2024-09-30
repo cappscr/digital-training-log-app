d700a457694d0d6efc2800d67f2b365ae126902b
cappscr <capps.christopher@gmail.com>
2024-09-30 08:26:33 -0400

    Heading updates

--- digital-training-log/app/index.tsx
+++ digital-training-log/app/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text } from "../components/ui/text";
 import { Box } from "../components/ui/box";
+import { Heading } from '../components/ui/heading';
+import { Icon } from '../components/ui/icon';
+import { Calculator } from 'lucide-react-native';
 
 export default function Index() {
   return (
     <Box
       className="flex-1"
     >
+      <Heading size="3xl">Pace Calculator
+        <Icon className="text-typography-500" as={Calculator} />
+      </Heading>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </Box>
   );
