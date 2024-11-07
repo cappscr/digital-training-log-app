1f4a8c23f8187d1168a4a30762073b6d7de745b0
cappscr <capps.christopher@gmail.com>
2024-11-07 06:20:44 -0500

    DTL-25 Adds the form tag and basic mantine form setup

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import {useState} from 'react';
 import {
   Button,
   Center,
@@ -9,13 +12,32 @@ import {
   Text,
   Title,
 } from "@mantine/core";
+import { useForm } from "@mantine/form";
 
 export default function HomePage() {
+  interface paceCalculatorState {
+    pace: string,
+    percentage: number
+  }
+
+  const [inputPace, setInputPace] = useState(null);
+
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: {
+      'pace-min': 1,
+      'pace-sec': 0,
+      'pace-units': 'min/mi',
+      percentage: 1,
+    }
+  });
+
   return (
     <Container size="sm">
       <Center>
         <Title order={1}>Pace Calculator</Title>
       </Center>
+      <form onSubmit={form.onSubmit((values) => console.log(values))}>
       <Group>
         <NumberInput
           maw={100}
@@ -28,6 +50,8 @@ export default function HomePage() {
           stepHoldInterval={100}
           min={0.01}
           max={59.59}
+          key={form.key('pace-min')}
+          {...form.getInputProps('pace-min')}
         />
         <Text fw={700} mt="1rem">:</Text>
         <NumberInput
@@ -39,11 +63,15 @@ export default function HomePage() {
           allowDecimal={false}
           min={0}
           max={59}
+          key={form.key('pace-sec')}
+          {...form.getInputProps('pace-sec')}
         />
         <NativeSelect
           label=" "
           data={['min/mi', 'km/mi']}
           radius="md"
+          key={form.key('pace-units')}
+          {...form.getInputProps('pace-units')}
           />
       </Group>
       <Stack align="flex-start">
@@ -56,9 +84,12 @@ export default function HomePage() {
         min={1}
         max={99}
         suffix="%"
+        key={form.key('percentage')}
+        {...form.getInputProps('percentage')}
       />
-      <Button radius="md">Calculate</Button>
+      <Button type="submit" radius="md">Calculate</Button>
       </Stack>
+      </form>
     </Container>
   );
 }
