504e32673cd020b3ada634e56830e9b2b157a3c4
cappscr <capps.christopher@gmail.com>
2025-02-16 22:03:58 -0500

    DTL-31 Adds the express package setup to help handle http requests

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,21 @@
-console.log("Hello, Digital Training Log App");
+import { createServer } from "http";
+import express, { Express, urlencoded } from "express";
+import helmet from "helmet";
+
+const port = 8000;
+
+const expressApp: Express = express();
+
+expressApp.use(helmet());
+expressApp.use(express.json());
+expressApp.use(urlencoded({ extended: true }));
+
+expressApp.get("/", (req, resp) => {
+  resp.send("Hello, Digital Training Log App");
+});
+
+const server = createServer(expressApp);
+
+server.listen(port, 
+  () => console.log(`HTTP Server listening on port ${port}`)
+);
