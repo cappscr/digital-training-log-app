6ed6510e1a4a2bfa6458baaabf47dd0724722c0b
cappscr <capps.christopher@gmail.com>
2026-02-16 05:26:48 -0500

    Fixes a linting error

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 8}
+  validates :password, presence: true, length: { minimum: 8 }
 end
