65e8686e93d5edcecc4cc60692e05f081b38817e
cappscr <capps.christopher@gmail.com>
2026-03-20 05:57:32 -0400

    [#32] Renames and moves PageTitle to be a direct child of components folder

--- frontend/src/components/PageTitle.test.tsx
+++ frontend/src/components/PageTitle.test.tsx
@@ -1,7 +1,7 @@
 import { expect, test } from 'vitest';
 import { render } from '../../vitest.setup';
 import { waitFor } from '@testing-library/dom';
-import { PageTitle } from './';
+import { PageTitle } from '../PageTitle';
 
 test('should set the title to the default when no pageName prop is passed', () => {
   render(<PageTitle />);
