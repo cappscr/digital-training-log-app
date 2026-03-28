aea0472da541dc60649c052365cf98fc3a3b4f1f
cappscr <capps.christopher@gmail.com>
2026-03-27 23:42:59 -0400

    [#34] Cleans up a couple of additional value based classes for Tailwind defaults

--- frontend/src/pages/TermsOfUse/components/TermsTypography.tsx
+++ frontend/src/pages/TermsOfUse/components/TermsTypography.tsx
@@ -1,7 +1,5 @@
 import { type ReactNode } from 'react';
 
 export const TermsTypography = ({ children }: { children: ReactNode }) => {
-  return (
-    <p className="text=[0.97rem] text-body-text mb-4 last:mb-0">{children}</p>
-  );
+  return <p className="text-base text-body-text mb-4 last:mb-0">{children}</p>;
 };
