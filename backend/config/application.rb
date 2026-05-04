daa6b78f37d835f4451a952906169351ff937000
cappscr <capps.christopher@gmail.com>
2026-05-04 06:07:48 -0400

    [#66] Adds cookie middlewares to config and creates login and logout routes

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -29,6 +29,9 @@ module Backend
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
     config.generators do |g|
       g.test_framework :rspec
       g.integration_tool :rspec
