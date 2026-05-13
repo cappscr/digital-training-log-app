1d9c6c6918225b448d4bb7cca54f54547b65e578
cappscr <capps.christopher@gmail.com>
2026-05-13 06:27:16 -0400

    [#97] Fixes InvalidTokenError class

--- backend/app/errors/invalid_token_error.rb
+++ backend/app/errors/invalid_token_error.rb
@@ -1,4 +1,4 @@
-class TokenExpiredError < ApplicationError
+class InvalidTokenError < ApplicationError
   problem_type "https://api.digitaltraininglog.com/problems/invalid-token-error"
   problem_title "Invalid Token"
   problem_status 401
