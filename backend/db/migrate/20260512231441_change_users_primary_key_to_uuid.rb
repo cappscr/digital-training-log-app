e7a1e9efbc80412abe92d62ab6aa082b5a9a5d9e
cappscr <capps.christopher@gmail.com>
2026-05-13 21:14:23 -0400

    [#97] Fixes ChangeUsersPrimaryKeyToUuid migration to run in production

--- backend/db/migrate/20260512231441_change_users_primary_key_to_uuid.rb
+++ backend/db/migrate/20260512231441_change_users_primary_key_to_uuid.rb
@@ -1,10 +1,14 @@
 class ChangeUsersPrimaryKeyToUuid < ActiveRecord::Migration[8.1]
-  def change
+  def up
     remove_column :users, :id
-    add_column :users, :id, :string, primary_key: true, null: false
+    add_column :users, :id, :string, null: true
+    execute "UPDATE users SET id = gen_random_uuid() WHERE id IS NULL"
+    change_column_null :users, :id, false
+    execute "ALTER TABLE users ADD PRIMARY KEY (id)"
   end
 
   def down
+    execute "ALTER TABLE users DROP CONSTRAINT users_pkey"
     remove_column :users, :id
     add_column :users, :id, :integer, primary_key: true, null: false
   end
