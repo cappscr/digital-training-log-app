91ee93c12ed1bdfddf0cf9e3a0ee23527d28d8d4
cappscr <capps.christopher@gmail.com>
2026-06-27 07:03:20 -0400

    [#188] Fixes rubocop errors

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -55,16 +55,16 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: ENV['FRONTEND_URL'] }
+  config.action_mailer.default_url_options = { host: ENV["FRONTEND_URL"] }
 
   config.action_mailer.delivery_method = :smtp
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   config.action_mailer.smtp_settings = {
-    address: ENV['SMTP_ADDRESS'],
+    address: ENV["SMTP_ADDRESS"],
     port: 587,
-    domain: ENV['SMTP_DOMAIN'],
-    user_name: ENV['SMTP_USERNAME'],
-    password: ENV['SMTP_PASSWORD'],
+    domain: ENV["SMTP_DOMAIN"],
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
     authentication: :plain,
     enable_starttls_auto: true
   }
