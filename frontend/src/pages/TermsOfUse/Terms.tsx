f05377eac59a29c6c179458382b362f1acafe7db
cappscr <capps.christopher@gmail.com>
2026-06-29 06:01:43 -0400

    [#203] Adds pageNames for About and Terms of Use pages

--- frontend/src/pages/TermsOfUse/Terms.tsx
+++ frontend/src/pages/TermsOfUse/Terms.tsx
@@ -1,3 +1,4 @@
+import { PageTitle } from '@/components/PageTitle';
 import { TermsHeader } from './components/TermsHeader';
 import { TermsBody } from './components/TermsBody';
 import { TermsFooter } from './components/TermsFooter';
@@ -8,6 +9,7 @@ const EFFECTIVE_DATE = 'March 24, 2026';
 export const TermsOfUse = () => {
   return (
     <>
+      <PageTitle pageName="Terms of Use" />
       <TermsHeader effectiveDate={EFFECTIVE_DATE} />
       <div
         className="bg-border mx-auto mt-14 mb-0 h-px max-w-170"
