f7bde87e949a5651fd176edd7b822581a6d19ed1
cappscr <capps.christopher@gmail.com>
2026-06-29 05:51:58 -0400

    [#203] Updates the subject line and email address for the account_activation mailer

--- backend/app/mailers/user_mailer.rb
+++ backend/app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   def account_activation(user)
     @user = user
     @activation_url = "#{ENV['FRONTEND_URL']}/activate?token=#{user.activation_token}&email=#{CGI.escape(user.email)}"
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: "Confirm your Digital Training Log App account"
   end
 end
