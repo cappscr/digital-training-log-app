8a24595a8bb956305b6219f7606aee28a45cfbc2
cappscr <capps.christopher@gmail.com>
2026-09-03 21:27:49 -0400

    [#316] Adds the latest workout and serializes the day of the week

--- backend/app/serializers/training_session_serializer.rb
+++ backend/app/serializers/training_session_serializer.rb
@@ -1,7 +1,13 @@
 class TrainingSessionSerializer < ActiveModel::Serializer
-  attributes :id, :duration, :location_type, :notes, :session_date, :session_time, :sport_details_type, :user_id, :created_at, :updated_at
+  attributes :id, :duration, :day_of_week, :location_type, :notes, :session_date, :session_time, :sport_details_type, :user_id, :created_at, :updated_at
 
   attribute :sport_details
+
+  def day_of_week
+    return nil unless object.session_date
+
+    object.session_date.strftime("%A")
+  end
 
   def session_time
     return nil unless object.session_time
