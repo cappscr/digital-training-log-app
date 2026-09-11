e87d8de6d60baf15289c3ddf19fe0426be08eef1
cappscr <capps.christopher@gmail.com>
2026-09-11 05:31:02 -0400

    [#357] Reworks the 404 page using the shadcn empty component

--- frontend/src/pages/NotFound/NotFound.tsx
+++ frontend/src/pages/NotFound/NotFound.tsx
@@ -1,12 +1,10 @@
-import { RuledLines } from './components/RuledLines';
-import { Content } from './components/Content';
+import { EmptyPageNotFound } from './components/EmptyPageNotFound';
 import { Folio } from './components/Folio';
 
 export const NotFoundPage = () => {
   return (
     <main className="bg-background relative flex h-dvh min-h-150 flex-col items-center justify-center overflow-hidden px-8 py-12">
-      <RuledLines />
-      <Content />
+      <EmptyPageNotFound />
       <Folio />
     </main>
   );
--- frontend/src/pages/NotFound/components/JournalMark.tsx
+++ frontend/src/pages/NotFound/components/JournalMark.tsx
@@ -1,7 +1,7 @@
 export const JournalMark = () => {
   return (
     <svg
-      className="mb-10 h-16 w-16"
+      className="size-14"
       viewBox="0 0 64 64"
       xmlns="http://www.w3.org/2000/svg"
     >
