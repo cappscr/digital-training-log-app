22f4fa2deac944d3d27ccb055156cf2798298fa4
cappscr <capps.christopher@gmail.com>
2026-09-19 17:31:23 -0400

    [#375] Updates the model spec for cross training sessions with the new validations

--- backend/app/models/cross_training_session.rb
+++ backend/app/models/cross_training_session.rb
@@ -1,3 +1,15 @@
 class CrossTrainingSession < ApplicationRecord
   include TrainingSessionable
+  include DistanceValidatable
+  include DurationOrDistanceValidatable
+
+  normalizes :activity, with: ->(value) { value&.strip }
+
+  # Move to concern in #410
+  enum :elevation_unit, { ft: "ft", m: "m" }, validate: { allow_nil: true }
+  validates :elevation_gain, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
+  validates :elevation_unit, presence: true, if: -> { elevation_gain.present? }
+
+  validates :activity, presence: true, length: { maximum: 100 }
+  validates :average_heart_rate, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
 end
--- backend/spec/models/cross_training_session_spec.rb
+++ backend/spec/models/cross_training_session_spec.rb
@@ -29,4 +29,81 @@ RSpec.describe CrossTrainingSession, type: :model do
       expect(CrossTrainingSession.exists?(cross_training_session.id)).to be false
     end
   end
+
+  describe "validations" do
+    it "is invalid without an activity" do
+      cross_training_session = build(:cross_training_session, activity: nil)
+      expect(cross_training_session).not_to be_valid
+    end
+
+    it "is invalid with an activity longer than 100 characters" do
+      cross_training_session = build(:cross_training_session, activity: "a" * 101)
+      expect(cross_training_session).not_to be_valid
+    end
+
+    it "is invalid with a whitespace only activity" do
+      cross_training_session = build(:cross_training_session, activity: "  ")
+      expect(cross_training_session).not_to be_valid
+    end
+
+    it "is valid without elevation_gain" do
+      cross_training_session = build(:cross_training_session, elevation_gain: nil)
+      expect(cross_training_session).to be_valid
+    end
+
+    it "is invalid with an elevation_gain less than 0" do
+      cross_training_session = build(:cross_training_session, elevation_gain: -1)
+      expect(cross_training_session).not_to be_valid
+    end
+
+    it "is invalid with invalid elevation_unit" do
+      cross_training_session = build(:cross_training_session, elevation_unit: "invalid")
+      expect(cross_training_session).not_to be_valid
+    end
+
+    it "is invalid with elevation_gain but no elevation_unit" do
+      cross_training_session = build(:cross_training_session, elevation_gain: 100, elevation_unit: nil)
+      expect(cross_training_session).not_to be_valid
+    end
+
+    it "is invalid with an average_heart_rate less than 0" do
+      cross_training_session = build(:cross_training_session, average_heart_rate: 0)
+      expect(cross_training_session).not_to be_valid
+    end
+
+    it "is invalid with a distance less than or equal to 0" do
+      cross_training_session = build(:cross_training_session, distance: 0)
+      expect(cross_training_session).not_to be_valid
+    end
+
+    it "is invalid with a distance but no distance_unit" do
+      cross_training_session = build(:cross_training_session, distance: 10, distance_unit: nil)
+      expect(cross_training_session).not_to be_valid
+    end
+
+    it "is invalid with invalid distance_unit" do
+      cross_training_session = build(:cross_training_session, distance_unit: "invalid")
+      expect(cross_training_session).not_to be_valid
+    end
+
+    it "is valid with duration and no distance" do
+      cross_training = build(:cross_training_session, :minimal)
+      cross_training.build_training_session(duration_seconds: 1000, user: build(:user))
+      expect(cross_training).to be_valid
+    end
+
+    it "is invalid without a distance or duration" do
+      cross_training = build(:cross_training_session, distance: nil)
+      training_session = build(:training_session, duration_seconds: nil, sport_details: cross_training)
+      expect(cross_training).not_to be_valid
+      expect(cross_training.errors.full_messages).to include("Duration and distance can't both be blank")
+    end
+  end
+
+  describe "normalizations" do
+    it "normalizes activity" do
+      cross_training_session = build(:cross_training_session, activity: "  test  ")
+      expect(cross_training_session.activity).to eq("test")
+    end
+  end
 end
