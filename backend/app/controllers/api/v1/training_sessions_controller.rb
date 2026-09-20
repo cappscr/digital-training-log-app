bf0852d0a85644945806b1721565065af51ecfe7
cappscr <capps.christopher@gmail.com>
2026-09-20 06:25:17 -0400

    [#375] Updates controller and makes sure all existing tests pass

--- backend/app/controllers/api/v1/training_sessions_controller.rb
+++ backend/app/controllers/api/v1/training_sessions_controller.rb
@@ -14,17 +14,17 @@ module Api
       def update
         training_session = current_user.training_sessions.find(params[:id])
         training_session.assign_attributes(update_params.except(:sport_details))
-        training_session.sport_details.assign_attributes(sport_details_params(update_params).except(:kind, :id))
+        # Pass in the existing sport_details.kind so that the update action cannot mutate it
+        training_session.sport_details.assign_attributes(update_sport_details(update_params, training_session.sport_details_type))
         training_session.save!
         render json: training_session, serializer: TrainingSessionSerializer
       end
 
       def create
         training_session = current_user.training_sessions.build(create_params.except(:sport_details))
-        # condition the sport details based on the params[:sport_details][:kind]
-        details = RunningTrainingSession.new(sport_details_params(create_params).except(:kind))
-        training_session.sport_details = details
-        details.training_session = training_session
+        sport_details = build_sport_details(create_params)
+        training_session.sport_details = sport_details
+        sport_details.training_session = training_session
         training_session.save!
         render json: training_session, status: :created
       end
@@ -42,6 +42,28 @@ module Api
 
       private
 
+      def build_sport_details(params)
+        raise ActionController::ParameterMissing, :sport_details if params[:sport_details].nil?
+        case params[:sport_details][:kind]
+        when "running"
+          RunningTrainingSession.new(running_create_params(params).except(:kind))
+        when "cross_training"
+          CrossTrainingSession.new(cross_training_params(params).except(:kind))
+        else
+          raise BadRequestError, "Unknown sport kind: #{params[:sport_details][:kind]}"
+        end
+      end
+
+      def update_sport_details(params, kind)
+        raise ActionController::ParameterMissing, :sport_details if params[:sport_details].nil?
+        case kind
+        when "RunningTrainingSession"
+          running_update_params(update_params)
+        when "CrossTrainingSession"
+          cross_training_params(update_params).except(:id)
+        end
+      end
+
       def create_params
         params.expect(
           training_session: [
@@ -54,9 +76,11 @@ module Api
             sport_details: [
               :id,
               :kind,
+              :activity,
               :distance,
               :distance_unit,
               :elevation_gain,
+              :elevation_unit,
               :average_cadence,
               :average_heart_rate
             ]
@@ -64,7 +88,7 @@ module Api
         )
       end
 
-      def sport_details_params(root)
+      def running_create_params(root)
         root.expect(
           sport_details: [
             :id,
@@ -73,6 +97,33 @@ module Api
             :distance_unit,
             :elevation_gain,
             :average_cadence,
+            :average_heart_rate
+          ]
+        )
+      end
+
+      def running_update_params(root)
+        root.expect(
+          sport_details: [
+            :distance,
+            :distance_unit,
+            :elevation_gain,
+            :average_cadence,
+            :average_heart_rate
+          ]
+        )
+      end
+
+      def cross_training_params(root)
+        root.expect(
+          sport_details: [
+            :id,
+            :kind,
+            :activity,
+            :distance,
+            :distance_unit,
+            :elevation_gain,
+            :elevation_unit,
             :average_heart_rate
           ]
         )
